a21991c7fe4f1d9a9acc62e831803c5e52ded841
MdIbrahimUllah <jeasan0007@gmail.com>
2022-08-20 11:03:33 -0400

    adding js function to html

---
+++ script.js
@@ -0,0 +1,7 @@
+function clickAlert() {
+    
+    alert("You are in the Home Page");
+    // document.getElementById("body").style.backgroundColor = "orange";
+    // alert("click home to go back to normal");
+  
+  }
