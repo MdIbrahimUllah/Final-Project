4b451f114e864d8140727300b2dd4552be93efce
MdIbrahimUllah <jeasan0007@gmail.com>
2022-08-20 14:24:41 -0400

    adding navbar,icons and update design of work page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function clickAlert() {
     
-    alert("You are in the Home Page");
+    alert("You are at the Home Page");
     // document.getElementById("body").style.backgroundColor = "orange";
     // alert("click home to go back to normal");
   
