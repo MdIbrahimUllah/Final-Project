9ee37683c8075d48f9224c715b917f0846d2807a
MdIbrahimUllah <jeasan0007@gmail.com>
2022-08-20 23:03:12 -0400

    adding js to construction work page

---
+++ construction.js
@@ -0,0 +1,14 @@
+// message when click on my work or work link.
+function clickAlert (){
+    alert("You are at my work page");
+}
+
+// message when click on Sign in link.
+function clickAlert2 (){
+    alert("Usually it will directed you to Sign in page");
+}
+
+// message when click on Sign up link.
+function clickAlert3 (){
+    alert("Usually it will directed you to Sign up page");
+}
