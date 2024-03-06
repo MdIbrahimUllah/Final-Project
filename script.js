fd1fad74c20725385b8c001d8ca2ff401bb84843
MdIbrahimUllah <jeasan0007@gmail.com>
2024-03-06 18:22:05 -0500

    Updated Home Page

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function clickAlert() {
     // alert("click home to go back to normal");
   
   }
+
+  // creating a send-mail function
+  function sendMail(){
+    let parms={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    }
+    emailjs.send("service_x85q6i8", "template_fivsp3u", parms).then(alert("Email has been sent successfully!"))
+}
